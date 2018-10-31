622fe9c105929efb6a9b9248243f0472d5ab2f0c
AlexFreeman412 <alexfreeman@emailinbox.me>
2018-10-31 20:37:40 +1100

    Added LEFT, RIGHT and HELP commands

--- Toy-Robot/Program.cs
+++ Toy-Robot/Program.cs
@@ -11,7 +11,7 @@ namespace ToyRobot
         static void Main()
         {
             while (!exitNextLoop)
-            {
+            { 
                 Console.WriteLine("Type your command");
 
                 var input = Console.ReadLine();
@@ -23,27 +23,43 @@ namespace ToyRobot
                 switch (command)
                 {
                     case "PLACE":
-                        // Gets the x, y and directional input from the input string and stores them in individual variables
-                        var parameters = input.Split(new char[] { ' ' }, 2)[1];
-
-                        if (validatePLACEParameters(parameters))
+                        // Checks that something follows the PLACE command and if not shows an error message
+                        if(input.Split(new char[] { ' ' }, 2).Length < 2)
                         {
-                            int x = Convert.ToInt32(parameters.Split(new char[] { ',' }, 3)[0]);
-                            int y = Convert.ToInt32(parameters.Split(new char[] { ',' }, 3)[1]);
-                            var direction = parameters.Split(new char[] { ',' }, 3)[2];
-                            placeRobot(x, y, direction);
+                           Console.WriteLine("The PLACE command must be followed by an x coordinate, y coordinate and direction seperated by commas (eg. 1,2,NORTH)");
                         }
                         else
                         {
-                            Console.WriteLine("The PLACE command must be followed by an x coordinate, y coordinate and direction seperated by commas (eg. 1,2,NORTH)");
-                        }
-                        
+                            // Gets the x, y and directional input from the input string and stores them in individual variables
+                            var parameters = input.Split(new char[] { ' ' }, 2)[1];
+
+                            if (validatePLACEParameters(parameters))
+                            {
+                                int x = Convert.ToInt32(parameters.Split(new char[] { ',' }, 3)[0]);
+                                int y = Convert.ToInt32(parameters.Split(new char[] { ',' }, 3)[1]);
+                                var direction = parameters.Split(new char[] { ',' }, 3)[2];
+                                placeRobot(x, y, direction);
+                            }
+                            else
+                            {
+                                Console.WriteLine("The PLACE command must be followed by an x coordinate, y coordinate and direction seperated by commas (eg. 1,2,NORTH)");
+                            }
+                        }                        
                         break;
                     case "MOVE":
                         moveRobot();
                         break;
+                    case "LEFT":
+                        rotateRobotLeft();
+                        break;
+                    case "RIGHT":
+                        rotateRobotRight();
+                        break;
                     case "REPORT":
                         report();
+                        break;
+                    case "HELP":
+                        printHelp();
                         break;
                     case "EXIT":
                         exitNextLoop = true;
@@ -52,10 +68,14 @@ namespace ToyRobot
                         Console.WriteLine(input + " is not a valid command.");
                         break;
                 }
+
+                Console.WriteLine();
             }
             
         }
 
+        // If there is no robot on the table this places the robot at the specified position facing the specified direction. 
+        // If there is already a robot on the table this changes the robots position and facing direction to those specified.
         private static bool placeRobot(int x, int y, string direction)
         {
             if(robot == null)
@@ -67,6 +87,7 @@ namespace ToyRobot
             return true;
         }
 
+        // Attempts to move the robot based on its current facing position and gives the appropriate message if not able to.
         private static bool moveRobot()
         {
             if (robot == null)
@@ -84,6 +105,31 @@ namespace ToyRobot
             return true;
         }
 
+        // Attempts to rotate the robot left and gives the appropriate message if not able to.
+        private static bool rotateRobotLeft()
+        {
+            if (robot == null)
+            {
+                Console.WriteLine("No robot on table");
+                return false;
+            }
+
+            return robot.Left();
+        }
+
+        // Attempts to rotate the robot right and gives the appropriate message if not able to.
+        private static bool rotateRobotRight()
+        {
+            if (robot == null)
+            {
+                Console.WriteLine("No robot on table");
+                return false;
+            }
+
+            return robot.Right();
+        }
+
+        // Attempts to generate and display a report showing the robots current x and y positions and it's facing direction in the following format "x,y,direction" and gives the appropriate message if not able to. 
         private static bool report()
         {
             if (robot == null)
@@ -96,6 +142,24 @@ namespace ToyRobot
             return true;
         }
 
+        // This method just prints some instructions of valid commands for the user's reference.
+        private static void printHelp()
+        {
+            Console.WriteLine("The following are valid commands: ");
+            Console.WriteLine("PLACE x,y,DIRECTION - places the robot on the table at the specified location and facing the specified direction (eg. PLACE 1,2,NORTH).");
+            Console.WriteLine("Valid x coordinates are 0-4.");
+            Console.WriteLine("Valid x coordinates are 0-4.");
+            Console.WriteLine("Valid directions are NORTH, EAST, SOUTH and WEST.");
+            Console.WriteLine("MOVE - Moves the robot based on which direction it is currently facing.");
+            Console.WriteLine("LEFT - Rotates the robot to face in the direction left of where it is currently facing.");
+            Console.WriteLine("RIGHT - Rotates the robot to face in the direction right of where it is currently facing.");
+            Console.WriteLine("REPORT - Shows the robots current location and which direction it is facing in the format x,y,DIRECTION.");
+            Console.WriteLine("HELP - Displays this help information.");
+            Console.WriteLine("EXIT - Exits the application.");
+        }
+
+        // Following a PLACE command we expect input in the format of a x coordinate, y coordinate and valid direction seperated by commas (eg. PLACE 1,2,NORTH).
+        // The following code checks to ensure we have received valid input and if not returns false. 
         private static bool validatePLACEParameters(string parameters)
         {
             bool isOK = Regex.IsMatch(parameters, @"[0-9]\,[0-9]\,");
--- Toy-Robot/Robot.cs
+++ Toy-Robot/Robot.cs
@@ -3,17 +3,18 @@ namespace ToyRobot
 {
     public class Robot
     {
-        private int xPos; // valid x coordinate positions are 0 - 5
-        private int yPos; // valid y coordinate positions are 0 - 5
+        private int xPos; // valid x coordinate positions are 0 - 4
+        private int yPos; // valid y coordinate positions are 0 - 4
         private string direction;
 
         public Robot()
         {
         }
 
+        // Places the robot on the table, if the robot is being placed off the table it will return false.
         public bool Place(int x, int y, string facing)
         {
-            if (x < 0 || x > 5 || y < 0 || y > 5)
+            if (x < 0 || x > 4 || y < 0 || y > 4)
                 return false;
 
             xPos = x;
@@ -23,13 +24,15 @@ namespace ToyRobot
             return true;
         }
 
+        // Moves the robot one unit in the direction it is facing (unless that movement would move the robot off the table in which case it makes no movement and returns a value of false).
         public bool Move()
         {
-            // Checks if the attempted move will move the robot off the table (eg. an x or y value of -1 or 6) and if so does not make the move. If it is a valid move then make the move and return true.
+            // Checks if the attempted move will move the robot off the table (eg. an x or y value of -1 or 5) and if so does not make the move. If it is a valid move then make the move and return true.
+            // Valid x and y coordinates are 0-4 because instructions specify origin at 0,0 and a table of 5 units x 5 units.
             switch (direction)
             {
                 case "NORTH":
-                    if(yPos + 1 > 5)
+                    if(yPos + 1 > 4)
                     {
                         return false;
                     }
@@ -49,7 +52,7 @@ namespace ToyRobot
                         return true;
                     }
                 case "EAST":
-                    if (xPos + 1 > 5)
+                    if (xPos + 1 > 4)
                     {
                         return false;
                     }
@@ -74,6 +77,55 @@ namespace ToyRobot
             }
         }
 
+        // Rotates the robot 90 degrees left (eg. if robot is facing NORTH it will face WEST after rotating)
+        public bool Left()
+        {
+            switch (direction)
+            {
+                case "NORTH":
+                    direction = "WEST";
+                    break;
+                case "WEST":
+                    direction = "SOUTH";
+                    break;
+                case "SOUTH":
+                    direction = "EAST";
+                    break;
+                case "EAST":
+                    direction = "NORTH";
+                    break;
+                default:
+                    // SHOULD NEVER HAPPEN
+                    return false;
+            }
+            return true;
+        }
+
+        // Rotates the robot 90 degrees right (eg. if robot is facing NORTH it will face EAST after rotating)
+        public bool Right()
+        {
+            switch (direction)
+            {
+                case "NORTH":
+                    direction = "EAST";
+                    break;
+                case "EAST":
+                    direction = "SOUTH";
+                    break;
+                case "SOUTH":
+                    direction = "WEST";
+                    break;
+                case "WEST":
+                    direction = "NORTH";
+                    break;
+                default:
+                    // SHOULD NEVER HAPPEN
+                    return false;
+            }
+            return true;
+        }
+
+        // Returns a string of the position of the robot along the x and y axis of the table and the direction the robot is facing in the format (x,y,direction).
         public string Report()
         {
             return xPos + "," + yPos + "," + direction;
