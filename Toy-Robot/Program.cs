55f78ddb61172a20f14ff6b4cdd0d9aac1294c0e
AlexFreeman412 <alexfreeman@emailinbox.me>
2018-10-31 20:07:03 +1100

    Added the MOVE command and added some validation around the PLACE command

--- Toy-Robot/Program.cs
+++ Toy-Robot/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Text.RegularExpressions;
+
 namespace ToyRobot
 {
     class Program
@@ -23,10 +25,22 @@ namespace ToyRobot
                     case "PLACE":
                         // Gets the x, y and directional input from the input string and stores them in individual variables
                         var parameters = input.Split(new char[] { ' ' }, 2)[1];
-                        int x = Convert.ToInt32(parameters.Split(new char[] { ',' }, 3)[0]);
-                        int y = Convert.ToInt32(parameters.Split(new char[] { ',' }, 3)[1]);
-                        var direction = parameters.Split(new char[] { ',' }, 3)[2];
-                        placeRobot(x, y, direction);
+
+                        if (validatePLACEParameters(parameters))
+                        {
+                            int x = Convert.ToInt32(parameters.Split(new char[] { ',' }, 3)[0]);
+                            int y = Convert.ToInt32(parameters.Split(new char[] { ',' }, 3)[1]);
+                            var direction = parameters.Split(new char[] { ',' }, 3)[2];
+                            placeRobot(x, y, direction);
+                        }
+                        else
+                        {
+                            Console.WriteLine("The PLACE command must be followed by an x coordinate, y coordinate and direction seperated by commas (eg. 1,2,NORTH)");
+                        }
+                        
+                        break;
+                    case "MOVE":
+                        moveRobot();
                         break;
                     case "REPORT":
                         report();
@@ -35,7 +49,7 @@ namespace ToyRobot
                         exitNextLoop = true;
                         break;
                     default:
-                        //TODO: Error 
+                        Console.WriteLine(input + " is not a valid command.");
                         break;
                 }
             }
@@ -53,14 +67,53 @@ namespace ToyRobot
             return true;
         }
 
+        private static bool moveRobot()
+        {
+            if (robot == null)
+            {
+                Console.WriteLine("No robot on table");
+                return false;
+            }
+
+            if (!robot.Move())
+            {
+                Console.WriteLine("Robot can not move off of the table. Type REPORT to see where the robot is currently positioned.");
+                return false;
+            }
+
+            return true;
+        }
+
         private static bool report()
         {
             if (robot == null)
             {
+                Console.WriteLine("No robot on table");
                 return false;
             }
 
             Console.WriteLine(robot.Report());
+            return true;
+        }
+
+        private static bool validatePLACEParameters(string parameters)
+        {
+            bool isOK = Regex.IsMatch(parameters, @"[0-9]\,[0-9]\,");
+            if (!isOK)
+            {
+                return false;
+            }
+            int n;
+            var xisNumeric = int.TryParse(parameters.Split(new char[] { ',' }, 3)[0], out n);
+            int z;
+            var yisNumeric = int.TryParse(parameters.Split(new char[] { ',' }, 3)[1], out z);
+            var direction = parameters.Split(new char[] { ',' }, 3)[2];
+
+            if(!xisNumeric || !yisNumeric || (!direction.Equals("NORTH") && !direction.Equals("SOUTH") && !direction.Equals("EAST") && !direction.Equals("WEST")))
+            {
+                return false;
+            }
+
             return true;
         }
 
--- Toy-Robot/Robot.cs
+++ Toy-Robot/Robot.cs
@@ -23,6 +23,57 @@ namespace ToyRobot
             return true;
         }
 
+        public bool Move()
+        {
+            // Checks if the attempted move will move the robot off the table (eg. an x or y value of -1 or 6) and if so does not make the move. If it is a valid move then make the move and return true.
+            switch (direction)
+            {
+                case "NORTH":
+                    if(yPos + 1 > 5)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        yPos++;
+                        return true;
+                    }
+                case "SOUTH":
+                    if (yPos - 1 < 0)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        yPos--;
+                        return true;
+                    }
+                case "EAST":
+                    if (xPos + 1 > 5)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        xPos++;
+                        return true;
+                    }
+                case "WEST":
+                    if (xPos - 1 < 0)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        xPos--;
+                        return true;
+                    }
+                default:
+                    // Should never happen!
+                    return false;
+            }
+        }
+
         public string Report()
         {
             return xPos + "," + yPos + "," + direction;
